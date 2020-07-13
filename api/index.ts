33edbdc64bfe6308104697fdc85f794862e5fee8
qixiong.li 李其雄 <qixiong.li@qiyuesuo.com>
2020-07-13 09:57:29 +0800

    queryBatchList

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,7 @@
-export class ApiContractBatch {
+import axios from 'axios';
 
+export class ApiContractBatch {
+    static queryBatchList(params) {
+        return axios.get('/batchsign/query', { params });
+    }
 }
